2dbe7eeccb9ba16b3821911988d8373031e26f0c
Ulf Tallmyr <ulf.tallmyr@gmail.com>
2026-08-09 12:29:54 +0200

    Initial commit

---
+++ custom_components/vasttrafik_departures/config_flow.py
@@ -0,0 +1,45 @@
+"""Config flow for the Västtrafik integration."""
+
+from __future__ import annotations
+
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_NAME
+from homeassistant.data_entry_flow import FlowResult
+
+from .const import CONF_ORIGIN, CONF_TOWARDS, DOMAIN
+
+
+class VasttrafikConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self,
+        user_input: dict[str, Any] | None = None,
+    ) -> FlowResult:
+        """Handle the initial step."""
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title=user_input[CONF_NAME],
+                data=user_input,
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_NAME,
+                        default="Västtrafik",
+                    ): str,
+                    vol.Required(CONF_ORIGIN): str,
+                    vol.Required(CONF_TOWARDS): str,
+                }
+            ),
+        )
---
+++ custom_components/vasttrafik_departures/const.py
@@ -0,0 +1,18 @@
+"""Constants for the Västtrafik integration."""
+
+DOMAIN = "vasttrafik_departures"
+
+NAME = "Västtrafik Departures"
+
+DEFAULT_NAME = "Västtrafik"
+
+DEFAULT_UPDATE_INTERVAL = 60
+DEFAULT_NUMBER_OF_DEPARTURES = 4
+
+CONF_ORIGIN = "origin"
+CONF_TOWARDS = "towards"
+CONF_DEPARTURES = "departures"
+CONF_UPDATE_INTERVAL = "update_interval"
+
+CONF_CLIENT_ID = "client_id"
+CONF_CLIENT_SECRET = "client_secret"
